a98355201b26c4af9b141a20ce0ab632682e0aec
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2021-05-04 20:21:35 -0300

    Ajuste do arquivo main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ Bootstrap(app)
 # app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL?sslmode=require').replace('postgres://', 'postgresql://')#os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
