8ced6f477cc5fc562a273154510b7af3f3547974
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2021-05-04 19:56:12 -0300

    "Ajuste do arquivo main.py para rodar localmente"

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ from functools import wraps
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")#'8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
